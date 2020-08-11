1dc8bbd922c7298e4eaf943da21c223b3121649a
Jake Taylor <65513095+jakehtaylor@users.noreply.github.com>
2020-08-11 18:38:23 -0400

    app final

--- app.py
+++ app.py
@@ -20,13 +20,21 @@ def home():
     print("homepage accessed")
     return render_template('home.html')
 
-@app.route("/api/v1.0/precipitation")
-def prcp():
+@app.route("/api/v1.0/precipitation/pretty")
+def prcp_p():
     session = Session(engine)
     last_twelve = session.query(Measure.date, Measure.prcp).filter(Measure.date > '2016-08-22').all()
     session.close()
     last_twelve = dict(last_twelve)
     return render_template('prcp.html', _data_ = (last_twelve))
+
+@app.route("/api/v1.0/precipitation/json")
+def prcp_j():
+    session = Session(engine)
+    last_twelve = session.query(Measure.date, Measure.prcp).filter(Measure.date > '2016-08-22').all()
+    session.close()
+    last_twelve = dict(last_twelve)
+    return jsonify(last_twelve)
 
 @app.route("/api/v1.0/stations")
 def stations():
@@ -37,8 +45,8 @@ def stations():
     return jsonify(sts)
     
 
-@app.route("/api/v1.0/tobs")
-def tobs():
+@app.route("/api/v1.0/tobs/pretty")
+def tobs_p():
     session = Session(engine)
     _stations = session.query(Measure.station, func.count(Measure.station)).group_by(
     Measure.station).order_by(func.count(Measure.station).desc()).all()
@@ -49,6 +57,20 @@ def tobs():
     session.close()
     d = {most_act : list(t_obs)}
     return render_template('tobs.html', _data_=(d))
+
+
+@app.route("/api/v1.0/tobs/json")
+def tobs_j():
+    session = Session(engine)
+    _stations = session.query(Measure.station, func.count(Measure.station)).group_by(
+    Measure.station).order_by(func.count(Measure.station).desc()).all()
+    most_act = _stations[0][0]
+    t_obs = session.query(Measure.tobs).filter(
+    Measure.station == most_act).filter(
+    Measure.date > '2016-08-22').all() 
+    session.close()
+    d = {most_act : list(t_obs)}
+    return jsonify(d)
     
 @app.route("/api/v1.0/<start>")
 def start(start):
